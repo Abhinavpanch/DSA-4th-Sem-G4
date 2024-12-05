3ca5b1760933210e00f36d7a7bc9a46a66bc41e3
Abhinavpanch <absinghpanchal.01@gmail.com>
2024-12-05 17:38:02 +0530

    1s commit

---
+++ src/App.java
@@ -0,0 +1,26 @@
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+// public class Abhi{
+//     public static void main(String[] args){
+//         System.out.println("Hello World!");
+//         int myvariable =10;
+//         int my_variable1 =30;
+//         int $myvar =40;
+//         System.out.println(myvariable);
+//         System.out.println(my_variable1);
+//         System.out.println($myvar);
+//     }
+// }
+
+public class App {
+    public static void main(String[] args) {
+        int [] arr = {10, 20, 30, 40, 50};
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Element at index " + i + " is " + arr[i]);
+        }
+    }
+}
